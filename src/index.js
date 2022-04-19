6736ff356ebdb785ffbc3e882198076912a2b07b
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-04-19 00:27:17 -0600

    feat: add theme file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from '../src/styles/theme'
+
 import App from './App'
 import AboutUs from './pages/AboutUs'
 import Book from './pages/Book'
@@ -16,19 +20,22 @@ const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="about-us" element={<AboutUs />} />
-          <Route path="features" element={<Features />} />
-          <Route path="location" element={<Location />} />
-          <Route path="rates" element={<Rates />} />
-          <Route path="book" element={<Book />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="features" element={<Features />} />
+            <Route path="location" element={<Location />} />
+            <Route path="rates" element={<Rates />} />
+            <Route path="book" element={<Book />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 )
