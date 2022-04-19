3bab84476600c216e8066e39cb7136785f53f6a6
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-04-19 00:13:15 -0600

    feat/add react router (#8)
    PR close #8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './App'
+import AboutUs from './pages/AboutUs'
+import Book from './pages/Book'
+import Contact from './pages/Contact'
+import Features from './pages/Features'
+import Home from './pages/Home'
+import Location from './pages/Location'
+import NotFound from './pages/NotFound'
+import Rates from './pages/Rates'
 
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="about-us" element={<AboutUs />} />
+          <Route path="features" element={<Features />} />
+          <Route path="location" element={<Location />} />
+          <Route path="rates" element={<Rates />} />
+          <Route path="book" element={<Book />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
--- src/pages/Rates/index.js
+++ src/pages/Rates/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Rate = () => {
+const Rates = () => {
   return <h1>This is the Rate</h1>
 }
 
-export default Rate
+export default Rates
