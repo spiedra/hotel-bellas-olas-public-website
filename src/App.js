115413dba7db70ac2ad6784003c54bafc16a41e3
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-04-19 00:34:27 -0600

    feat: update app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+import Box from '@mui/material/Box'
+import Navbar from './components/Navbar'
+
+const drawerWidth = 300
 
 function App () {
   return (
-    <h1>This is the app</h1>
+    <Box sx={{ display: 'flex', height: '100vh' }}>
+      <Navbar />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          paddingTop: 6,
+          paddingLeft: 2,
+          width: { sm: `calc(100% - ${drawerWidth}px)` }
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
   )
 }
 
