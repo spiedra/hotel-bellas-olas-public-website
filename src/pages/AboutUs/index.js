3fbcd43dcb0a39e067081a8430f71fc56e14b02b
Randall <randms07@hotmail.com>
2022-04-22 18:28:42 -0600

    feat: add about us section

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Gallery, GalleryPicture, DisplayImg, ImageContainer } from './styles'
+import { getAboutUsInfo } from '../../services/Gets/getAboutUsInfo'
+import { LoaderSpinner } from '../../components/Loader'
 
 const AboutUs = () => {
-  return <h1>This is the about us</h1>
+  const [aboutUsInfo, setAboutUsInfo] = useState()
+  const [imgDisplay, setImgDisplay] = useState()
+
+  useEffect(() => {
+    getAboutUsInfo().then((response) => {
+      setAboutUsInfo(response)
+      setImgDisplay(response ? response.imgList[0] : '')
+    })
+  }, [])
+
+  const onDisplayImage = (img) => {
+    setImgDisplay(img)
+  }
+
+  return (
+    <>
+      <h1>Sobre nosotros</h1>
+      <p>{aboutUsInfo ? aboutUsInfo.aboutUsText : 'texto sobre nosotros texto sobre nosotros texto sobre nosotros texto sobre nosotros texto sobre nosotros texto sobre nosotros'}</p>
+      {aboutUsInfo
+        ? (
+          <Gallery>
+            <DisplayImg src={imgDisplay}></DisplayImg>
+            <ImageContainer>
+            {
+              aboutUsInfo.imgList.map((img, index) => (
+                    <GalleryPicture key={index} onClick={() => onDisplayImage(img)} src={img}/>
+              )
+              )
+            }
+            </ImageContainer>
+          </Gallery>
+          )
+        : (
+        <LoaderSpinner/>
+          )}
+    </>
+  )
 }
 
 export default AboutUs
