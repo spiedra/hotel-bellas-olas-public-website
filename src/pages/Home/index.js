86bd17e8fb698f34cc753cff907953a5e99ee342
María F. Orocú Arias <mari.oro923@gmail.com>
2022-04-22 18:41:56 -0600

    Feat/add home section (#20)
    * feat: Add text from home section
    
    * feat:add home section 
    
    close #20

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Div, Img } from './styles'
+import { getHomeInfo } from '../../services/Gets/getHomeInfo'
 
 const Home = () => {
-  return <h1>This is the Home</h1>
-}
+  const [homeInfo, setHomeInfo] = useState()
 
+  useEffect(() => {
+    getHomeInfo().then((response) => {
+      console.log(response)
+      setHomeInfo(response)
+    })
+  }, [])
+
+  return (
+    <>
+    <Div>
+    {homeInfo
+      ? (
+         <Img src={homeInfo.img ? homeInfo.img : ''} alt={homeInfo.alt ? homeInfo.alt : 'Hotel Bellas Olas Img'} />
+        )
+      : (<h2>Cargando</h2>)
+    }
+    <div style={{ marginLeft: '30px' }}>
+    <h1>Bienvenido a Hotel Bellas Olas </h1>
+      <p>{homeInfo ? homeInfo.homeText : 'Nuestra vista al mar le dará el descanso que usted merece. Rodeado de naturaleza y aguas cristalinas, Bellas Olas es el mejor lugar para desconectarse de la rutina y reconectar con la paz.'}</p>
+    </div>
+      </Div>
+      </>
+  )
+}
 export default Home
