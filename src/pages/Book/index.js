283450c24d66965b213374b3b84b606e26332105
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-05-03 14:32:53 -0600

    feat: add booking form

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,7 +1,115 @@
 import React from 'react'
 
+import { Controller, useForm } from 'react-hook-form'
+
+import { Box, Grid, MenuItem, Button, TextField } from '@mui/material'
+import { learningStyles } from './styles'
+
 const Book = () => {
-  return <h1>This is the book</h1>
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      tipoHabitacion: '',
+      start: '',
+      end: ''
+    }
+  })
+
+  const onSubmit = (values) => {
+    console.log(values)
+  }
+
+  return (
+    <Box sx={learningStyles.instructionsContainer}>
+      <h1>Reservar en Linea</h1>
+      <Box
+        component="form"
+        my="3rem"
+        sx={{
+          width: { xs: '100%', md: '70%' },
+          '& .MuiTextField-root': {
+            my: 1,
+            width: { xs: '37ch', md: '41ch' }
+          }
+        }}
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Grid
+          container
+          justifyContent="flex-start"
+          spacing={{ xs: 0.5, sm: 0.5, md: 2 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          <Grid item>
+            <Controller
+              control={control}
+              name="start"
+              rules={{ required: true }}
+              render={({ field: { ...field } }) => (
+                <TextField
+                  sx={learningStyles.select}
+                  {...field}
+                  type="date"
+                  error={!!errors.start}
+                  label="Fecha de Llegada"
+                  InputLabelProps={{ shrink: true }}
+                ></TextField>
+              )}
+            />
+          </Grid>
+          <Grid item>
+            <Controller
+              control={control}
+              name="end"
+              rules={{ required: true }}
+              render={({ field: { ...field } }) => (
+                <TextField
+                  sx={learningStyles.select}
+                  {...field}
+                  type="date"
+                  error={!!errors.end}
+                  InputLabelProps={{ shrink: true }}
+                  label="Fecha de Salida"
+                ></TextField>
+              )}
+            />
+          </Grid>
+          <Grid item>
+            <Controller
+              control={control}
+              name="tipoHabitacion"
+              rules={{ required: true }}
+              render={({ field: { ...field } }) => (
+                <TextField
+                  sx={learningStyles.select}
+                  {...field}
+                  select
+                  error={!!errors.tipoHabitacion}
+                  label="Tipo de Habitación"
+                >
+                  <MenuItem value="1">Estandar</MenuItem>
+                  <MenuItem value="2">Junior</MenuItem>
+                </TextField>
+              )}
+            />
+          </Grid>
+        </Grid>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          sx={learningStyles.button}
+          onClick={handleSubmit}
+        >
+          Aceptar
+        </Button>
+      </Box>
+    </Box>
+  )
 }
 
 export default Book
