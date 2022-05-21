801673b462e5653d7806644f792b780e7ec996b3
María F. Orocú Arias <mari.oro923@gmail.com>
2022-05-20 23:17:09 -0600

    add contact section (#34)
    close #33

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Contact = () => {
-  return <h1>This is the Contact</h1>
+  return (
+  <div>
+   <h1>Contáctenos</h1>
+  <h4>Hotel Bellas Olas</h4>
+  <p>Tel:22520034 / 22520035</p>
+  <p>Apdo postal: 61101</p>
+  <p>info@bellasolas.com</p>
+  </div>
+  )
 }
 
 export default Contact
