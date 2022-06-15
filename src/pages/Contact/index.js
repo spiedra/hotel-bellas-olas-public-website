93b72e2b9d63bfe0a264dc0fba69a3a78bbb6161
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-06-15 12:01:14 -0600

    style: update contact styles

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,14 +1,32 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 const Contact = () => {
   return (
-  <div>
-   <h1>Contáctenos</h1>
-  <h4>Hotel Bellas Olas</h4>
-  <p>Tel:22520034 / 22520035</p>
-  <p>Apdo postal: 61101</p>
-  <p>info@bellasolas.com</p>
-  </div>
+    <Box sx={{ mt: '3rem', ml: '1.5rem' }}>
+      <h1>Contáctenos</h1>
+      <Box sx={{ ml: '.5rem' }}>
+        <h3>Hotel Bellas Olas</h3>
+        <p>
+          <Box component="span" sx={{ fontWeight: 'bold', display: 'inline' }}>
+            Tel:
+          </Box>{' '}
+          22520034 / 22520035
+        </p>
+        <p>
+          <Box component="span" sx={{ fontWeight: 'bold', display: 'inline' }}>
+            Código postal:
+          </Box>{' '}
+          61101
+        </p>
+        <p>
+          <Box component="span" sx={{ fontWeight: 'bold', display: 'inline' }}>
+            Correo electrónico:
+          </Box>{' '}
+          info@bellasolas.com
+        </p>
+      </Box>
+    </Box>
   )
 }
 
--- src/pages/Rates/index.js
+++ src/pages/Rates/index.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react'
 
-import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material'
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@mui/material'
 import { LoaderSpinner } from '../../components/Loader'
 import { GetRoomRates } from '../../services/Gets/getRoomRate'
 
@@ -14,23 +21,17 @@ const Rates = () => {
   return (
     <Box sx={{ mt: '3rem', ml: '1.5rem' }}>
       <h1>Tarifas</h1>
-      <Box sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: '40px'
-      }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '40px'
+        }}
+      >
         {rates
           ? (
           <>
             {rates.map((item, index) => (
-              // <RateItem key={index}>
-              //   <RateImage src={rate.img} />
-              //   <RateDescription>
-              //     <h2>Habitación {rate.category}</h2>
-              //     {rate.description.trim()}
-              //     <RateCost>Precio regular: ₡{rate.cost}</RateCost>
-              //   </RateDescription>
-              // </RateItem>
               <Card key={index} sx={{ maxWidth: 345 }}>
                 <CardActionArea>
                   <CardMedia
@@ -40,9 +41,9 @@ const Rates = () => {
                     alt={`Imagen de ${item.category}`}
                   />
                   <CardContent>
-                  <Typography gutterBottom variant="h5" component="h5">
-                        {item.category}
-                      </Typography>
+                    <Typography gutterBottom variant="h5" component="h5">
+                      {item.category}
+                    </Typography>
                     <Typography
                       variant="body2"
                       color="text.secondary"
@@ -52,7 +53,12 @@ const Rates = () => {
                     </Typography>
                     <Typography
                       variant="body1"
-                      sx={{ lineHeight: '28px', fontSize: '15px', mt: '1rem', fontWeight: 'bold' }}
+                      sx={{
+                        lineHeight: '28px',
+                        fontSize: '15px',
+                        mt: '1rem',
+                        fontWeight: 'bold'
+                      }}
                     >
                       Precio regular: ₡{item.cost}
                     </Typography>
@@ -63,7 +69,7 @@ const Rates = () => {
           </>
             )
           : (
-            <LoaderSpinner />
+          <LoaderSpinner />
             )}
       </Box>
     </Box>
